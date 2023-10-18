a4269028e79f2ba6f72ca99483703c1651abbf47
Jonathon Carter <30672425+jnoc@users.noreply.github.com>
2023-10-18 20:32:58 +0100

    Update teapot.js

--- .netlify/functions/teapot.js
+++ .netlify/functions/teapot.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
   const response = {
     statusCode: 418,
-    body: "I'm a teapot",
+    body: "I'm a teapot, have a cookie!",
     headers: {
       "Content-Type": "text/plain",
       "Set-Cookie": "teapot=true; Max-Age=3600; HttpOnly; Secure"
